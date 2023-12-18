e2d2df9a32c48797876c023fb3369ddda5b8afe5
Michael Celia <mcelia2324@gmail.com>
2023-12-18 13:32:58 -0600

    cleaning up main file + update to gitignore

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from loopring.account import Account
 from loopring.exchange import Exchange
 from loopring.fees import Fees, OffchainRequestType
 
-"""
+
 # Set your API key and account ID
 creds = {'api_key': 'YOUR_API_KEY', 'account_id': 'YOUR_ACCOUNT_ID'}
 
 # Save your creds to a serialized pickle file
 with open('loopringCreds.pickle', 'wb') as f:
     pickle.dump(creds, f)
-"""
 
 
 # Load the creds file and set the API key and account ID
@@ -33,8 +32,3 @@ exchange = Exchange()
 tokens = exchange.get_markets()
 for i in tokens:
     print(i)
-
-fee = Fees()
-fees = fee.get_erc20_offchain_fees(OffchainRequestType.UPDATE_ACCOUNT)
-print(fees)
-
